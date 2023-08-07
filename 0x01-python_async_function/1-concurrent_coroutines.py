01d19177e826c797de0f349a2904f5f759273314
kate mwende <kmwende419@gmail.com>
2023-08-07 18:34:51 +0300

    Error checking

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,15 +3,15 @@
 write an async routine that takes 2 arguments(n and max_delay)
 wait_n should return the list of all the delays (float values)
 """
+
 import asyncio
-import random
 from heapq import nsmallest
 from typing import List
 
-wait_random = __import__("0-basic_async_syntax").wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random n times with the specified max_delay"""
     delays_list = []
     for i in range(n):
