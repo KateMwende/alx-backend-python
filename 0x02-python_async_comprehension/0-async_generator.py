03febb297d11d5af073384723756c470a554c63e
kate mwende <kmwende419@gmail.com>
2023-08-08 20:50:27 +0300

    Removed random_list

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,5 +12,4 @@ async def async_generator() -> AsyncIterator[float]:
     """yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        random_list = random.uniform(0, 10)
-        yield random_list
+        yield random.uniform(0, 10)
