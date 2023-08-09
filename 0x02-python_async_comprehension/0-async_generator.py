f6576b831fe5204acfc73cafc18eef9efc13c1f6
kate mwende <kmwende419@gmail.com>
2023-08-09 11:38:16 +0300

    Used Generator hint type instead

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ Coroutine will loop 10 times, each time asynchronously wait 1s
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import AsyncIterator, Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
