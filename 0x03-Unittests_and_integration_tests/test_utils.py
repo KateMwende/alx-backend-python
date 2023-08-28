313408c95fb6aa472ec9d2ca90d98d2b48a30503
kate mwende <kmwende419@gmail.com>
2023-08-28 18:37:09 +0300

    Test if json is returned from url

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Unit test for utlis file
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +31,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
             self.assertEqual(str(context.exception), str(exception_message))
+
+
+class TestGetJson(unittest.TestCase):
+    """Get ajson response from url test"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Make sure it returns a Mock object with a json method"""
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = test_payload
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
